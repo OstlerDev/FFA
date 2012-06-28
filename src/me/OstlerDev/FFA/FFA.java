59443b57d868bb84a74c6604cd6117c5abd51ed1
OstlerDev <ostlerdev@me.com>
2012-06-27 23:36:36 -0600

    Massive internal changes

--- src/me/OstlerDev/FFA/FFA.java
+++ src/me/OstlerDev/FFA/FFA.java
@@ -12,126 +12,197 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FFA extends JavaPlugin
 {
-  public static FFA plugin;
-  public final Logger logger = Logger.getLogger("Minecraft");
+	public static FFA plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
 
-  public HashMap<Player, Integer> wait6 = new HashMap<Player, Integer>();
-  public HashMap<Player, ItemStack[]> saveItems = new HashMap<Player, ItemStack[]>();
-  public HashMap<Player, Integer> playerKills = new HashMap<Player, Integer>();
+	public HashMap<Player, Integer> wait6 = new HashMap<Player, Integer>();
+	public HashMap<Player, ItemStack[]> saveItems = new HashMap<Player, ItemStack[]>();
+	public HashMap<Player, Location> saveLocation = new HashMap<Player, Location>();
+	public HashMap<Player, Integer> playerKills = new HashMap<Player, Integer>();
 
-  public void onDisable()
-  {
-    saveConfig();
-  }
+	public void onDisable()
+	{
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+		saveConfig();
+	}
 
-  public void onEnable()
-  {
-    PluginManager pm = getServer().getPluginManager();
-    pm.registerEvents(new listener(this), this);
-    plugin = this;
-  }
+	public void onEnable()
+	{
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + ", Version "
+				+ pdfFile.getVersion() + ", Has Been Enabled!");
+		PluginManager pm = getServer().getPluginManager();
+		EntityDamageEvent.getHandlerList().unregister(com.massivecraft.factions.P.p.entityListener);
+		pm.registerEvents(new listener(this), this);
+		plugin = this;
 
-  public int random(int x, int y) {
-    if (x < y) {
-      Random r = new Random();
-      return x + r.nextInt(y - x + 1);
-    }
-    return 1;
-  }
+		getServer().getScheduler().scheduleAsyncRepeatingTask(this,
+				new Runnable()
+				{
+					public void run()
+					{
+						plugin.getServer().broadcastMessage("Dev Preview by: OstlerDev");
+					}
+				}, 60L, 1200L);
+	}
 
-  public void rTeleport(final Player p)
-  {
-    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
- 
-    		public void run() {
-    			plugin.saveItems.put(p, p.getInventory().getContents());
-    			    p.sendMessage(ChatColor.GREEN + "Your inventory has been saved until after the game!");
-    			    plugin.wait6.remove(p);
-    			    Location l = null;
-    			    int i = random(1, 20);
-    			    String arenaloc = (String)getConfig().get("FFA.spawn." + i);
-    			    if (getConfig().get("FFA.spawn." + i) == null) return;
-    			    String[] arenalocarr = arenaloc.split(",");
-    			    if (arenalocarr.length == 3) {
-    			      int x = Integer.parseInt(arenalocarr[0]);
-                      int y = Integer.parseInt(arenalocarr[1]);
-    			      int z = Integer.parseInt(arenalocarr[2]);
-    			      l = new Location(getServer().getWorld("world"), x, y, z);
-    			    }
-    						
-    			    p.teleport(l);
-    			    Inventory b = p.getInventory();
-                    b.clear();
-    			    ItemStack sword = new ItemStack(Material.IRON_SWORD);
-    			    ItemStack food = new ItemStack(Material.ROTTEN_FLESH, 16);
-    			    ItemStack axe = new ItemStack(Material.IRON_AXE);
-    			    b.addItem(new ItemStack[] { sword, food, axe });
-    			    p.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
-    			    p.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
-                    p.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
-    			    p.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
-                    plugin.playerKills.put(p, Integer.valueOf(0));
-    }
-}, 120L);
-  }
+	public int random(int x, int y)
+	{
+		if (x < y)
+		{
+			Random r = new Random();
+			return x + r.nextInt(y - x + 1);
+		}
+		return 1;
+	}
 
-  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-     if ((sender instanceof Player)) {
-       Player p = (Player)sender;
-       if ((commandLabel.equalsIgnoreCase("ffa")) && (!this.playerKills.containsKey(p)) && (p.hasPermission("FFA.default"))) {
-         if (args.length == 0) {
-           p.sendMessage(ChatColor.GREEN + "You are about to enter the arena, please wait 6 seconds");
-			if (p.getGameMode() == GameMode.CREATIVE) p.sendMessage("Switching you to Survival!");
+	public void rTeleport(final Player p)
+	{
+		int i = Bukkit.getServer().getScheduler()
+				.scheduleSyncDelayedTask(this, new Runnable()
+				{
+
+					public void run()
+					{	
+						if (plugin.wait6.containsKey(p))
+						{
+							plugin.saveItems.put(p, p.getInventory()
+									.getContents());
+							p.sendMessage(ChatColor.GREEN
+									+ "Your inventory has been saved!");
+							plugin.saveLocation.put(p, p.getLocation());
+							plugin.wait6.remove(p);
+							Location l = null;
+							int i = random(1, 5);
+							String arenaloc = (String) getConfig().get(
+									"FFA.spawn." + i);
+							String[] arenalocarr = arenaloc.split(",");
+							if (arenalocarr.length == 3)
+							{
+								int x = Integer.parseInt(arenalocarr[0]);
+								int y = Integer.parseInt(arenalocarr[1]);
+								int z = Integer.parseInt(arenalocarr[2]);
+								l = new Location(getServer().getWorld("world"),
+										x, y, z);
+							}
+
+							p.teleport(l);
+							Inventory b = p.getInventory();
+							b.clear();
+							ItemStack sword = new ItemStack(Material.IRON_SWORD);
+							ItemStack food = new ItemStack(
+									Material.ROTTEN_FLESH, 16);
+							ItemStack axe = new ItemStack(Material.IRON_AXE);
+							b.addItem(new ItemStack[] { sword, food, axe });
+							p.getInventory().setChestplate(
+									new ItemStack(Material.IRON_CHESTPLATE));
+							p.getInventory().setLeggings(
+									new ItemStack(Material.IRON_LEGGINGS));
+							p.getInventory().setHelmet(
+									new ItemStack(Material.IRON_HELMET));
+							p.getInventory().setBoots(
+									new ItemStack(Material.IRON_BOOTS));
+							plugin.playerKills.put(p, Integer.valueOf(0));
+						}
+					}
+				}, 120L);
+		
+		plugin.wait6.put(p, i);
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args)
+	{
+		if ((sender instanceof Player))
+		{
+			Player p = (Player) sender;
+			if ((commandLabel.equalsIgnoreCase("ffa"))
+					&& (p.hasPermission("FFA.default")))
+			{
+				if(!this.playerKills.containsKey(p))
+				{
+				if (args.length == 0)
+				{
+					p.sendMessage(ChatColor.GREEN
+							+ "You are about to enter the arena, please wait 6 seconds");
+					if (p.getGameMode() == GameMode.CREATIVE)
+						p.sendMessage("Switching you to Survival!");
 					p.setGameMode(GameMode.SURVIVAL);
-					this.wait6.put(p, 600);
-           rTeleport(p);
-        }
-         else if (args.length == 1) {
-           if ((args[0].equalsIgnoreCase("reload")) && (p.hasPermission("FFA.*")))
-             reloadConfig();
-           else if (args[0].equalsIgnoreCase("lb")) p.hasPermission("FFA.*");
+					rTeleport(p);
+				}
+				else if (args.length == 1)
+				{
+					if ((args[0].equalsIgnoreCase("reload"))
+							&& (p.hasPermission("FFA.*")))
+						reloadConfig();
+					else if (args[0].equalsIgnoreCase("lb"))
+						p.hasPermission("FFA.*");
 
-        }
-         else if (args.length == 2) {
-           if ((args[0].equalsIgnoreCase("setspawn")) && (p.hasPermission("FFA.*"))) {
-             String a = args[1];
-			
-             if (!getConfig().contains("FFA.spawn." + a)) {
-               getConfig().set("FFA.spawn." + a, null);
-            }
+				}
+				else if (args.length == 2)
+				{
+					if ((args[0].equalsIgnoreCase("setspawn"))
+							&& (p.hasPermission("FFA.*")))
+					{
+						String a = args[1];
 
-             Location loc = p.getLocation();
-             int blockX = loc.getBlockX();
-             int blockY = loc.getBlockY();
-             int blockZ = loc.getBlockZ();
-             getConfig().set("FFA.spawn." + a, blockX + "," + blockY + "," + blockZ);
-             p.sendMessage(ChatColor.GOLD + "Spawn #" + a + " has been set succesfully at:" + blockX + ", " + blockY + ", " + blockZ);
-           } else if (args[0].equalsIgnoreCase("removespawn")) {
-             String a = args[1];
-             getConfig().set("FFA.spawn." + a, null);
-             p.sendMessage(ChatColor.GOLD + "FFA.spawn." + a + " have been set to null");
-          }
-        } else {
-           p.sendMessage(ChatColor.GREEN + "Invalid command!");
-        }
-      }
-       else p.sendMessage(ChatColor.GREEN + "You can't /ffa while in battle!");
-    }
+						if (!getConfig().contains("FFA.spawn." + a))
+						{
+							getConfig().set("FFA.spawn." + a, null);
+						}
 
-     return false;
-  }
-
-			public boolean wait360Ticks(Player player){
-				int i = this.wait6.get(player);
-				if (i == 0) return true;
-				else this.wait6.put(player, i--);
-				return false;
+						Location loc = p.getLocation();
+						int blockX = loc.getBlockX();
+						int blockY = loc.getBlockY();
+						int blockZ = loc.getBlockZ();
+						getConfig().set("FFA.spawn." + a,
+								blockX + "," + blockY + "," + blockZ);
+						p.sendMessage(ChatColor.GOLD + "Spawn #" + a
+								+ " has been set succesfully at:" + blockX
+								+ ", " + blockY + ", " + blockZ);
+					}
+					else if (args[0].equalsIgnoreCase("removespawn"))
+					{
+						String a = args[1];
+						getConfig().set("FFA.spawn." + a, null);
+						p.sendMessage(ChatColor.GOLD + "FFA.spawn." + a
+								+ " have been set to null");
+					}
+				}
+				else
+				{
+					p.sendMessage(ChatColor.RED + "Invalid command!");
+				}
+				}
+				else
+					p.sendMessage(ChatColor.RED
+							+ "You can't /ffa while in battle!");
 			}
+			else
+				p.sendMessage(ChatColor.RED
+						+ "You don't have permission for this command!");
+		}
+
+		return false;
+	}
+
+	/*public boolean wait360Ticks(Player player)
+	{
+		int i = this.wait6.get(player);
+		if (i == 0)
+			return true;
+		else
+			this.wait6.put(player, i--);
+		return false;
+	}*/
 }
--- src/me/OstlerDev/FFA/listener.java
+++ src/me/OstlerDev/FFA/listener.java
@@ -1,156 +1,289 @@
-/*     */ package me.OstlerDev.FFA;
-/*     */ 
-/*     */ import java.util.Map.Entry;
-/*     */ import org.bukkit.ChatColor;
-import org.bukkit.Location;
-/*     */ import org.bukkit.Material;
-/*     */ import org.bukkit.entity.Entity;
-/*     */ import org.bukkit.entity.HumanEntity;
-/*     */ import org.bukkit.entity.Player;
-/*     */ import org.bukkit.event.EventHandler;
-/*     */ import org.bukkit.event.Listener;
-/*     */ import org.bukkit.event.block.SignChangeEvent;
-/*     */ import org.bukkit.event.entity.PlayerDeathEvent;
-/*     */ import org.bukkit.event.inventory.InventoryClickEvent;
-/*     */ import org.bukkit.event.player.PlayerDropItemEvent;
-/*     */ import org.bukkit.event.player.PlayerMoveEvent;
-/*     */ import org.bukkit.event.player.PlayerQuitEvent;
-/*     */ import org.bukkit.event.player.PlayerRespawnEvent;
-/*     */ import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
-/*     */ 
-/*     */ public class listener
-/*     */   implements Listener
-/*     */ {
-/*  24 */   public static FFA ffa = null;
-/*     */ 
-/*     */   public listener(Plugin plugin) {
-/*  27 */     ffa = (FFA)plugin;
-/*     */   }
-/*     */   @EventHandler
-/*     */   public void onMove(PlayerMoveEvent event) {
-/*  32 */     Player p = event.getPlayer();
-/*  33 */     if (ffa.wait6.containsKey(p))
-/*  34 */       event.setCancelled(true);
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void onDeath(PlayerDeathEvent event) {
-/*  40 */     Entity e = event.getEntity();
-/*  41 */     event.getDrops().removeAll(event.getDrops());
-/*  42 */     if ((e instanceof Player)) {
-/*  43 */       Player killer = ((Player) e).getKiller();
-/*  44 */       if (killer != null) {
-/*  45 */         int x = (ffa.playerKills.get(killer)).intValue();
-				  String xString = Integer.toString(x);
-/*  46 */         killer.sendMessage(xString);
-/*  47 */         int newx = x + 1;
-				  xString = Integer.toString(newx);
-/*  48 */         killer.sendMessage(xString);
-/*  49 */         ffa.playerKills.remove(killer);
-/*  50 */         ffa.playerKills.put(killer, Integer.valueOf(newx));
-/*  51 */         killer.getInventory().addItem(new ItemStack[] { new ItemStack(Material.ROTTEN_FLESH, 16) });
-/*  52 */         if (newx == 1) {
-/*  53 */           killer.getInventory().getItem(0).setType(Material.DIAMOND_SWORD);
-/*  54 */           killer.getInventory().getItem(2).setType(Material.DIAMOND_AXE);
-/*  55 */         } else if (newx == 2) {
-/*  56 */           killer.getInventory().addItem(new ItemStack[] { new ItemStack(Material.BOW) });
-/*  57 */           killer.getInventory().addItem(new ItemStack[] { new ItemStack(Material.ARROW, 32) });
-/*  58 */         } else if ((newx == 3) || (newx == 6) || (newx == 9) || (newx == 12) || (newx == 15) || (newx == 18) || (newx == 21) || (newx == 24)) {
-/*  59 */           killer.sendMessage(ChatColor.GREEN + "You have won $150!");
-/*  60 */           ffa.getServer().dispatchCommand(ffa.getServer().getConsoleSender(), "eco give " + killer.getName() + " 150");
-/*  61 */           killer.getInventory().addItem(new ItemStack[] { new ItemStack(Material.ARROW, 32) });
-/*     */         }
-/*     */       }
-/*     */     }
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void respawn(PlayerRespawnEvent event) {
-/*  70 */     Player p = event.getPlayer().getPlayer();
-/*  71 */     if (ffa.saveItems.containsKey(p))
-/*  72 */       for (Entry<Player, ItemStack[]> entry : ffa.saveItems.entrySet()) {
-/*  73 */         Player pl = (Player)entry.getKey();
-/*  74 */         ItemStack[] a = (ItemStack[])entry.getValue();
-/*  75 */         if (pl == p) {
-/*  76 */           pl.getInventory().clear();
-/*  77 */           pl.getInventory().setContents(a);
-/*  78 */           ffa.saveItems.remove(pl);
-/*  79 */           ffa.playerKills.remove(pl);
-/*  80 */           pl.sendMessage(ChatColor.GREEN + "Your inventory has been restored!");
-/*     */         }
-/*     */       }
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void inventoryClick(InventoryClickEvent event) {
-/*  88 */     HumanEntity e = event.getWhoClicked();
-/*  89 */     if ((e instanceof Player)) {
-/*  90 */       Player p = ((Player)e).getPlayer();
-/*  91 */       if (ffa.playerKills.containsKey(p)) {
-/*  92 */         event.setCancelled(true);
-/*  93 */         p.sendMessage(ChatColor.GREEN + "You are not allowd to move items in your inventory while in FFA");
-/*     */       }
-/*     */     }
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void inventoryDrop(PlayerDropItemEvent event) {
-/* 100 */     Player p = event.getPlayer();
-/* 101 */     if (ffa.playerKills.containsKey(p)) {
-/* 102 */       event.setCancelled(true);
-/* 103 */       p.sendMessage(ChatColor.GREEN + "You are not allowd to drop items in FFA");
-/*     */     }
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void signPlace(SignChangeEvent event) {
-/* 109 */     int kills = 2342;
-/* 110 */     int deaths = 1239;
-/* 111 */     float kd = kills / deaths;
-/* 112 */     String playerName = "TomShar";
-/*     */ 
-/* 114 */     String one = "K:D";
-/* 115 */     String two = kills + ":" + deaths;
-/* 116 */     String three = Float.toString(kd);
-/* 117 */     if (event.getLine(0).contains("[lb1]")) {
-/* 118 */       event.setLine(0, playerName);
-/* 119 */       event.setLine(1, one);
-/* 120 */       event.setLine(2, two);
-/* 121 */       event.setLine(3, three);
-/*     */     }
-/*     */   }
-/*     */ 
-/*     */   @EventHandler
-/*     */   public void onQuit(PlayerQuitEvent event) {
-/* 127 */     Player p = event.getPlayer();
-				p.setHealth(20);
-				Location l = null;
-				if (ffa.playerKills.containsKey(p))
-					if (p.getBedSpawnLocation() != null)
-						l = p.getBedSpawnLocation();
-					else l = p.getCompassTarget();
-				p.teleport(l);
-				if (ffa.saveItems.containsKey(p))
-					for (Entry<Player, ItemStack[]> entry : ffa.saveItems.entrySet()) {
-						Player pl = (Player)entry.getKey();
-						ItemStack[] a = (ItemStack[])entry.getValue();
-						if (pl == p) {
-							pl.getInventory().clear();
-							pl.getInventory().setBoots(new ItemStack(Material.AIR));
-							pl.getInventory().setLeggings(new ItemStack(Material.AIR));
-							pl.getInventory().setChestplate(new ItemStack(Material.AIR));
-							pl.getInventory().setHelmet(new ItemStack(Material.AIR));
-							pl.getInventory().setContents(a);
-							ffa.saveItems.remove(pl);
-							ffa.playerKills.remove(pl);
-							pl.sendMessage(ChatColor.GREEN + "Your inventory has been restored!");
-					}
-					}
-/*     */   }
-/*     */ }
+package me.OstlerDev.FFA;
 
-/* Location:           /Users/Josh/Downloads/FFA.jar
- * Qualified Name:     me.TomShar.FFA.listener
- * JD-Core Version:    0.6.0
- */
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import com.massivecraft.factions.P;
+
+public class listener implements Listener
+{
+	public static FFA ffa = null;
+
+	public listener(Plugin plugin)
+	{
+		ffa = (FFA) plugin;
+	}
+
+	/*@EventHandler(priority = EventPriority.HIGH)
+	public void onEntityDamage(EntityDamageEvent event)
+	{	
+		if (event instanceof EntityDamageByEntityEvent)
+		{
+			EntityDamageByEntityEvent eevent = (EntityDamageByEntityEvent) event;
+
+			if (eevent.getDamager() instanceof Player)
+			{
+				if (ffa.saveItems.containsKey((Player) eevent.getDamager()))
+				{
+					for(RegisteredListener listener : event.getHandlers().getRegisteredListeners())
+					{
+						if(listener.getPlugin() instanceof P)
+						{
+							//toregister = listener;
+							event.getHandlers().unregister(listener);
+							Bukkit.getPluginManager().callEvent(event);
+							EntityDamageEvent.getHandlerList().register(listener);
+						}
+					}
+				}
+			}
+		}
+	}*/
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onEntityDamage(EntityDamageEvent event)
+	{
+		if (event.isCancelled()) return;
+
+		if (event instanceof EntityDamageByEntityEvent)
+		{
+			EntityDamageByEntityEvent sub = (EntityDamageByEntityEvent) event;
+			
+			if (sub.getEntity() instanceof Player)
+			{
+				Player attacker = null;
+				if(sub.getDamager() instanceof Player)
+					attacker = (Player) sub.getDamager();
+				if(sub.getDamager() instanceof Arrow && ((Arrow) sub.getDamager()).getShooter() instanceof Player)
+					attacker = (Player) ((Arrow) sub.getDamager()).getShooter();
+
+				if(attacker != null)
+				{
+					if (ffa.saveItems.containsKey((Player) sub.getDamager()) && ffa.saveItems.containsKey((Player) sub.getEntity()))
+					{
+						return;
+					}
+				}
+			}
+
+			if (!P.p.entityListener.canDamagerHurtDamagee(sub, true))
+			{
+				event.setCancelled(true);
+			}
+		}
+		// TODO: Add a no damage at all flag??
+		/*else if (Conf.safeZonePreventAllDamageToPlayers && isPlayerInSafeZone(event.getEntity()))
+		{
+			// Players can not take any damage in a Safe Zone
+			event.setCancelled(true);
+		}*/
+	}
+
+	/*@EventHandler(priority = EventPriority.LOW)
+	public void onEntityDamage2(EntityDamageEvent event)
+	{
+		if(toregister != null)
+		{
+			EntityDamageEvent.getHandlerList().register(toregister);
+			toregister = null;
+		}
+	}*/
+
+	@EventHandler
+	public void onMove(PlayerMoveEvent event)
+	{
+		Player p = event.getPlayer();
+
+		if (ffa.wait6.containsKey(p))
+		{
+			if (event.getFrom().getBlockX() != event.getTo().getBlockX()
+					|| event.getFrom().getBlockZ() != event.getTo().getBlockZ())
+			{
+				Bukkit.getScheduler().cancelTask(ffa.wait6.get(p));
+				ffa.wait6.remove(p);
+				event.getPlayer().sendMessage(
+						ChatColor.RED
+								+ "Teleport cancelled due to player movement.");
+			}
+		}
+	}
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event)
+	{
+		Entity e = event.getEntity();
+		event.getDrops().removeAll(event.getDrops());
+		if ((e instanceof Player))
+		{
+			Player killer = ((Player) e).getKiller();
+			if (killer != null)
+			{
+				int x = (ffa.playerKills.get(killer)).intValue();
+				//String xString = Integer.toString(x);
+				//killer.sendMessage(xString);
+				int newx = x + 1;
+				//xString = Integer.toString(newx);
+				//killer.sendMessage(xString);
+				ffa.playerKills.remove(killer);
+				ffa.playerKills.put(killer, Integer.valueOf(newx));
+				killer.getInventory().addItem(
+						new ItemStack[] { new ItemStack(Material.ROTTEN_FLESH,
+								16) });
+				if (newx == 1)
+				{
+					killer.getInventory().getItem(0)
+							.setType(Material.DIAMOND_SWORD);
+					killer.getInventory().getItem(2)
+							.setType(Material.DIAMOND_AXE);
+				}
+				else if (newx == 2)
+				{
+					killer.getInventory().addItem(
+							new ItemStack[] { new ItemStack(Material.BOW) });
+					killer.getInventory()
+							.addItem(
+									new ItemStack[] { new ItemStack(
+											Material.ARROW, 32) });
+				}
+				else if ((newx == 3) || (newx == 6) || (newx == 9)
+						|| (newx == 12) || (newx == 15) || (newx == 18)
+						|| (newx == 21) || (newx == 24))
+				{
+					killer.sendMessage(ChatColor.GREEN + "You have won $150!");
+					ffa.getServer().dispatchCommand(
+							ffa.getServer().getConsoleSender(),
+							"eco give " + killer.getName() + " 150");
+					killer.getInventory()
+							.addItem(
+									new ItemStack[] { new ItemStack(
+											Material.ARROW, 32) });
+				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void respawn(PlayerRespawnEvent event)
+	{
+		Player p = event.getPlayer().getPlayer();
+		if (ffa.saveItems.containsKey(p))
+		{
+			Entry<Player, ItemStack[]>[] entries = (Entry<Player, ItemStack[]>[]) ffa.saveItems.entrySet().toArray(new Entry[0]);
+			for (int i = 0;i<ffa.saveItems.entrySet().size();i++)
+			{
+				Entry<Player, ItemStack[]> entry = entries[i];
+				
+				Player pl = (Player) entry.getKey();
+				ItemStack[] a = (ItemStack[]) entry.getValue();
+				if (pl == p)
+				{
+					pl.getInventory().clear();
+					pl.getInventory().setContents(a);
+					ffa.saveItems.remove(pl);
+					ffa.playerKills.remove(pl);
+					pl.sendMessage(ChatColor.GREEN + "Your inventory has been restored!");
+				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void inventoryClick(InventoryClickEvent event)
+	{
+		HumanEntity e = event.getWhoClicked();
+		if ((e instanceof Player))
+		{
+			Player p = ((Player) e).getPlayer();
+			if (ffa.playerKills.containsKey(p))
+			{
+				event.setCancelled(true);
+				p.sendMessage(ChatColor.RED + "You are not allowed to move items in your inventory while in FFA.");
+			}
+		}
+	}
+
+	@EventHandler
+	public void inventoryDrop(PlayerDropItemEvent event)
+	{
+		Player p = event.getPlayer();
+		if (ffa.playerKills.containsKey(p))
+		{
+			event.setCancelled(true);
+			p.sendMessage(ChatColor.RED + "You are not allowed to drop items in FFA.");
+		}
+	}
+
+	@EventHandler
+	public void signPlace(SignChangeEvent event)
+	{
+		int kills = 2342;
+		int deaths = 1239;
+		float kd = kills / deaths;
+		String playerName = "TomShar";
+
+		String one = "K:D";
+		String two = kills + ":" + deaths;
+		String three = Float.toString(kd);
+		if (event.getLine(0).contains("[lb1]"))
+		{
+			event.setLine(0, playerName);
+			event.setLine(1, one);
+			event.setLine(2, two);
+			event.setLine(3, three);
+		}
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event)
+	{
+		Player p = event.getPlayer();
+		p.setHealth(20);
+		Location l = ffa.saveLocation.get(p);
+		p.teleport(l);
+		ffa.saveLocation.remove(p);
+		if (ffa.saveItems.containsKey(p))
+		{
+			Entry<Player, ItemStack[]>[] entries = (Entry<Player, ItemStack[]>[]) ffa.saveItems.entrySet().toArray(new Entry[0]);
+			for (int i = 0;i<ffa.saveItems.entrySet().size();i++)
+			{
+				Entry<Player, ItemStack[]> entry = entries[i];
+				Player pl = (Player) entry.getKey();
+				ItemStack[] a = (ItemStack[]) entry.getValue();
+				if (pl == p)
+				{
+					pl.getInventory().clear();
+					pl.getInventory().setBoots(new ItemStack(Material.AIR));
+					pl.getInventory().setLeggings(new ItemStack(Material.AIR));
+					pl.getInventory().setChestplate(new ItemStack(Material.AIR));
+					pl.getInventory().setHelmet(new ItemStack(Material.AIR));
+					pl.getInventory().setContents(a);
+					ffa.saveItems.remove(pl);
+					ffa.playerKills.remove(pl);
+					pl.sendMessage(ChatColor.GREEN + "Your inventory has been restored!");
+				}
+			}
+		}
+	}
+}
